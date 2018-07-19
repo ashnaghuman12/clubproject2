ec051c1d621900888d00c2ec4de765bb5525944c
ashnaghuman12 <40476545+ashnaghuman12@users.noreply.github.com>
2018-07-19 15:50:58 -0700

    Add files via upload

---
+++ business_project/Portfolio/main.js
@@ -0,0 +1,25 @@
+  alert("Hey! This is an alert box.");
+
+  var myButton = document.querySelector('button');
+  var myHeading = documetn.querySelector('h1');
+
+  function setUserName () {
+    var name = prompt('Please enter you name.');
+    myHeading.innerHTML()=name;
+
+  }
+
+  myButton.onclick = function() {
+    stUserName();
+
+  }
+
+  var MyImage = document.querySelector('img');
+
+  myImage.onclick = function(){
+    var myScr = myImage.getAtrribute('scr');
+
+    if(mySrc == 'download.jpg') {
+      myImage.setAttribute(scre)
+    }
+  }
